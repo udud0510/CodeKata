f94214f1c4c3b9e5038d86c3fa9d29ebf26b7bbb
udud <susan7744850@gmail.com>
2018-10-23 17:27:14 +0900

    프로그래머스 : 해쉬 : 전화번호 목록 : 2차시도 : 실패(한글변환)

--- src/yujin/programmers/hash/Solution2.java
+++ src/yujin/programmers/hash/Solution2.java
@@ -32,7 +32,7 @@ public class Solution2 {
 
         Arrays.sort(phone_book);
         for(int i=0;i<phone_book.length;i++){
-            for(int j=1;j<phone_book.length-1;j++){
+            for(int j=1;j<phone_book.length;j++){
                 if(phone_book[j].startsWith(phone_book[i])==true) {
                     answer = false;
                 }
