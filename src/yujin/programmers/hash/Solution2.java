45e23a7e297749dcb5f5f95d97f2d5010a589de1
udud <susan7744850@gmail.com>
2018-11-02 11:43:28 +0900

    프로그래머스 : 해쉬 : 전화번호 목록 : 2차시도 : 실패(한글변환)

--- src/yujin/programmers/hash/Solution2.java
+++ src/yujin/programmers/hash/Solution2.java
@@ -1,5 +1,6 @@
 package yujin.programmers.hash;
-
+import java.util.*;
+import java.util.HashMap;
 /*
         전화번호부에 적힌 전화번호 중, 한 번호가 다른 번호의 접두어인 경우가 있는지 확인하려 합니다.
         전화번호가 다음과 같을 경우, 구조대 전화번호는 영석이의 전화번호의 접두사입니다.
@@ -20,7 +21,6 @@ package yujin.programmers.hash;
         [123,456,789]	                true
         [12,123,1235,567,88]	        false
         */
-import java.util.Arrays;
 
 public class Solution2 {
     public boolean solution(String[] phone_book) {
@@ -30,9 +30,30 @@ public class Solution2 {
         // 2. 정렬된 배열의 앞문자만 비교해서 있으면 false 없으면 true
         //  - 자바 String 문법 공부
 
-        Arrays.sort(phone_book);
+        // 아래 코드를 입력했는데 Alt + Enter 하니 아래 코드로 변경 됨
+        // 프로그래머스에서는 1번 문제가 에러나는데 여기선 돌아가는 마법
+
+        /*List<HashMap<String, Object>> list = new ArrayList<HashMap<String, Object>>();
+        Collections.sort(list, new Comparator<HashMap<String, Object>>() {
+            @Override
+            public int compare(HashMap<String, Object> o1, HashMap<String, Object> o2) {
+                String name1 = (String) o1.get("phone_book");
+                String name2 = (String) o2.get("phone_book");
+                return name1.compareTo(name2);
+            }
+        });*/
+
+        List<HashMap<String, Object>> list = new ArrayList<>();
+        Collections.sort(list, (o1, o2) -> {
+            String name1 = (String) o1.get("phone_book");
+            String name2 = (String) o2.get("phone_book");
+            return name1.compareTo(name2);
+        });
+
+
         for(int i=0;i<phone_book.length;i++){
             for(int j=1;j<phone_book.length;j++){
+                if(i==j) continue;
                 if(phone_book[j].startsWith(phone_book[i])==true) {
                     answer = false;
                 }
