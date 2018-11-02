d12f17ed37b105c3c1d7ca4f5d9901d25c0a7141
udud <susan7744850@gmail.com>
2018-11-02 11:44:48 +0900

    프로그래머스 : 해쉬 : 전화번호 목록 : 해시 정렬 적용

--- src/yujin/programmers/hash/Solution2.java
+++ src/yujin/programmers/hash/Solution2.java
@@ -9,7 +9,8 @@ import java.util.HashMap;
         박준영 : 97 674 223
         지영석 : 11 9552 4421
         전화번호부에 적힌 전화번호를 담은 배열 phone_book 이 solution 함수의 매개변수로 주어질 때,
-        어떤 번호가 다른 번호의 접두어인 경우가 있으면 false를 그렇지 않으면 true를 return 하도록 solution 함수를 작성해주세요.
+        어떤 번호가 다른 번호의 접두어인 경우가 있으면 false를 그렇지 않으면 true를 return 하도록
+        solution 함수를 작성해주세요.
 
         제한 사항
         phone_book의 길이는 1 이상 1,000,000 이하입니다.
