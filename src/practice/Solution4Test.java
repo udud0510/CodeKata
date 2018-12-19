513c7caf1f2ab19d2ff172701e29863e975f4284
udud <susan7744850@gmail.com>
2018-12-19 15:36:29 +0900

    프로그래머스_문자열 내 마음대로 정렬하기

--- src/practice/Solution4Test.java
+++ src/practice/Solution4Test.java
@@ -1,4 +1,23 @@
 package practice;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
 public class Solution4Test {
+    @Test
+    public void 결과() {
+        Solution4 solution = new Solution4();
+
+        String[] strings={"sun","bed","car"};
+        int n=1;
+        String[] answer={"car","bed","sun"};
+
+        String[] strings2={"abce","abcd","cdx"};
+        int n2=2;
+        String[] answer2={"abcd","abce","cdx"};
+
+        assertArrayEquals(answer,solution.solution(strings,n));
+        assertArrayEquals(answer2,solution.solution(strings2,n2));
+    }
 }
