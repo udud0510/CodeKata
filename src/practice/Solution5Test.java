977a06af18809f80f9d77b3bcada86af5648760d
udud <susan7744850@gmail.com>
2018-12-19 15:36:51 +0900

    프로그래머스_문자열 내 p와y의 개수

---
+++ src/practice/Solution5Test.java
@@ -0,0 +1,14 @@
+package practice;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class Solution5Test {
+    @Test
+    public void 결과() {
+        Solution5 solution = new Solution5();
+
+        assertTrue("true",solution.solution("pPoooyY"));
+
+    }
+}
