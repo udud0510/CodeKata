fcab989d417bc2f97ed7d71b0e9e5c11d0b7d801
UD <susan7744850@gmail.com>
2019-05-19 21:50:17 +0900

    프로그래머스_예산

---
+++ src/level2/Solution2_3Test.java
@@ -0,0 +1,17 @@
+package level2;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Solution2_3Test {
+    @Test
+    public void 결과() {
+        Solution2_3 solution = new Solution2_3();
+        int[] array = {1, 3,2,5,4};
+        int[] array2 = {2,2,3,3};
+
+        assertEquals(3, solution.solution(array,9));
+        assertEquals(4, solution.solution(array2,10));
+    }
+}
