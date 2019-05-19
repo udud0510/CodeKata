65e14e99fa87f7c473be54568b758968571ecc55
UD <susan7744850@gmail.com>
2019-05-19 21:52:30 +0900

    알고리즘과제3

--- src/company/Question3.java
+++ src/company/Question3.java
@@ -5,10 +5,8 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
 
@@ -54,7 +52,6 @@ public class Question3 {
 
                 }
             }
-
             //정렬해서 출력
             for (Object o : sortByValue(realAnswer)) {
                 String temp = (String) o;
